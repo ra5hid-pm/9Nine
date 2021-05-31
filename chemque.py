dfaa20d0bb6393cf066b6a9821d34905b9963d9c
ra5hid-pm <84266742+ra5hid-pm@users.noreply.github.com>
2021-05-31 17:16:55 +0530

    Update chemque.py

--- chemque.py
+++ chemque.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-from IPython.display import display, Math, Latex                                                                                                                                                                                             
+from IPython.display import display, Math, Latex, Markdown                                                                                                                                                                                             
  #display(Math(r'F(k) = \int_{-\infty}^{\infty} f(x) e^{2\pi i k} dx'))                                                                                                                                                                     
  #display(Math(r'\ce{H2O}'))  
 
@@ -8,13 +8,13 @@ class mixque():
     def __init__():
         pass
     def queLatex():
-        return display(Math(r'\ce{H2O}')) 
+        display(Math(r'\ce{H2O}'))
     
     q1 = {"Q-1. " : "What is mixture?", 
           "  A. " : "Composition of more than one substance", 
           "  B. " : "Composition of more than two substance ", 
           "  C. " : "Composition of more than three substance ", 
-          "  D. " : queLatex() 
+          "  D. " : '$\ce{H2O}$'
          }
     q2 = {"Q-2. " : "State the types of mixture -",
           "  A. " : "Homogeneous", 
@@ -24,7 +24,14 @@ class mixque():
     def printque():
         
         for x in mixque.q1:
-            print(f'{x}{mixque.q1[x]}')
+            display(Markdown(rf'{x}{mixque.q1[x]}'))
+            
+        test_text = input ("Enter your option: ")
+        
+            
+                  
+            
+            
        
         
     def mixans(x):
@@ -33,3 +40,5 @@ class mixque():
             print ("Correct")
         else:
             print("Not correct")
+   
+   
