b34862ed836901dedf93d4f340638bbfdf35b65e
ra5hid-pm <84266742+ra5hid-pm@users.noreply.github.com>
2021-06-01 00:10:52 +0530

    Update chemque.py

--- chemque.py
+++ chemque.py
@@ -1,14 +1,18 @@
+# $$\require{mhchem}$$ 
 import numpy as np
 import pandas as pd
-from IPython.display import display, Math, Latex, Markdown                                                                                                                                                                                             
+from IPython.display import display, Math, Latex, Markdown   
+  
  #display(Math(r'F(k) = \int_{-\infty}^{\infty} f(x) e^{2\pi i k} dx'))                                                                                                                                                                     
  #display(Math(r'\ce{H2O}'))  
 
 class mixque():
     def __init__():
         pass
-    def queLatex():
-        display(Math(r'\ce{H2O}'))
+    score = 0
+    attempts ={ }
+    ans=["A", "B", "A"]
+    
     
     q1 = {"Q-1. " : "What is mixture?", 
           "  A. " : "Composition of more than one substance", 
@@ -16,29 +20,39 @@ class mixque():
           "  C. " : "Composition of more than three substance ", 
           "  D. " : '$\ce{H2O}$'
          }
+    
     q2 = {"Q-2. " : "State the types of mixture -",
           "  A. " : "Homogeneous", 
           "  B. " : "Hetrogeneous " 
          }
+    q3 = {" Q-3. " : " क्या हम हिंदी में काम कर सकते हैं ",
+          "  A. " : "No", 
+          "  B. " : "Not at all ", 
+          "  C. " : "Yes, Occasionally  ", 
+          "  D. " : 'Yes, Always'
+          
     
-    def printque():
+    }
+    que =[q1,q2,q3]
+    
+    def printque(y):
         
-        for x in mixque.q1:
-            display(Markdown(rf'{x}{mixque.q1[x]}'))
+        for x in mixque.que[y]:
+            display(Markdown(rf'{x}{mixque.que[y][x]}'))
             
-        test_text = input ("Enter your option: ")
+            
+        # Capture answer choice   
+        input_choice = input ("Enter your choice from the given options: ")
         
-            
-                  
-            
+        
+        # Answer response variable y ensures
+        if input_choice==mixque.ans[y]:
+            print("Correct")
+            mixque.attempts["Q"+str(y+1)] = "Right"
+            mixque.score = mixque.score+1
+        else:
+            print(f'{"Not Correct"}')
+            mixque.attempts["Q"+str(y+1)] = "Wrong"         
             
        
-        
-    def mixans(x):
-        a1 = 'A mixture is a composition of more than one pure substance.'
-        if x == a1:
-            print ("Correct")
-        else:
-            print("Not correct")
-   
    
