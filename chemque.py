fd50d1848804d950635444d3896495efc185d742
ra5hid-pm <84266742+ra5hid-pm@users.noreply.github.com>
2021-06-01 13:26:53 +0530

    Update chemque.py

--- chemque.py
+++ chemque.py
@@ -7,36 +7,57 @@ from IPython.display import display, Math, Latex, Markdown
  #display(Math(r'\ce{H2O}'))  
 
 class mixque():
-    def __init__():
+    def __init__(self,testkind):
+        self.tetskind = testkind
         pass
     score = 0
     attempts ={ }
-    ans=["A", "B", "A"]
+    
     
     
     q1 = {"Q-1. " : "What is mixture?", 
           "  A. " : "Composition of more than one substance", 
           "  B. " : "Composition of more than two substance ", 
           "  C. " : "Composition of more than three substance ", 
-          "  D. " : '$\ce{H2O}$'
+          "  D. " : 'Composition of atleast one substance'
          }
     
-    q2 = {"Q-2. " : "State the types of mixture -",
+    q2 = {"Q-2. " : "A mixture with uniform composition throughout is called-",
           "  A. " : "Homogeneous", 
-          "  B. " : "Hetrogeneous " 
+          "  B. " : "Hetrogeneous ",
+          "  C. " : "Saturated",
+          "  D. " : "Suspension"
          }
-    q3 = {" Q-3. " : " क्या हम हिंदी में काम कर सकते हैं ",
-          "  A. " : "No", 
-          "  B. " : "Not at all ", 
-          "  C. " : "Yes, Occasionally  ", 
-          "  D. " : 'Yes, Always'
+    q3 = {" Q-3. " : "Which of the following is a homogeneous mixture?",
+          "  A. " : "Copper Sulphate mixed with Common Salt", 
+          "  B. " : "Water mixed with oil ", 
+          "  C. " : "Iron filings mixed with common salt", 
+          "  D. " : 'Common salt mixed with water'
           
     
     }
-    que =[q1,q2,q3]
+    q4 = {" Q-4. " : "What is meant by saturated solution?",
+          "  A. " : "Solution which does not accept further solute at any temprature", 
+          "  B. " : "Solution which does not accept further solute at a given temprature", 
+          "  C. " : "Solution which allows the solute to absorb all the solvent ", 
+          "  D. " : 'Solution which do not accept further solute even if volume of solvent is increased'
+          
+    }
+    q5 = {" Q-5. " : """Four Students P, Q, R and S have been given Copper Sulphate, Common Salt, Wheat Flour and Ink respectively.
+          All of them have been asked to mix the given substance separately in a glass of water. Who will get a colloidal solution  """,
+          "  A. " : "P", 
+          "  B. " : "Q and R", 
+          "  C. " : "S", 
+          "  D. " : 'P and Q'
+          
+    }
+    que =[q1,q2,q3,q4,q5]
+    ans=["A", "A", "D", "B", "C"]
     
     def printque(y):
-        
+        # print a question with options from dictionary of questions as enumerated in the list que.
+        # This means that y will be the list item which are then searched through x for priniting the questions
+        # with its options.
         for x in mixque.que[y]:
             display(Markdown(rf'{x}{mixque.que[y][x]}'))
             
@@ -52,7 +73,10 @@ class mixque():
             mixque.score = mixque.score+1
         else:
             print(f'{"Not Correct"}')
-            mixque.attempts["Q"+str(y+1)] = "Wrong"         
+            mixque.attempts["Q"+str(y+1)] = "Wrong" 
+    def printkey():
+        for i in mixque.attempts:
+            print(i, mixque.attempts[i])
             
        
    
