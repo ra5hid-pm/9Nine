fc2c9d664a24c345b1b8c5c900c40e41484fae4b
ra5hid-pm <84266742+ra5hid-pm@users.noreply.github.com>
2021-06-09 23:54:16 +0530

    Update dictify.py

--- dictify.py
+++ dictify.py
@@ -1,9 +1,17 @@
 def mcqdictfy(string):
     y = string
+     #looping through the number of questions OR statement to handle two scenario
     for x in [1,2,3,4,5,6,7,8,9,10]:
-        y = y.replace(str(x)+". ", " ']},{' "+str(x)+":["+'"Q-'+str(x)+". ",2)
+        y = y.replace(str(x)+"." or str(x)+". ", '''"]},{'''+str(x)+":["+'"Q-'+str(x)+". ",2)
+    # looping through the options
     for k in ['(a)','(b)','(c)','(d)']:
         y = y.replace(k, '","'+k)
     y = y +'"]}]'
-    y = y.replace(y[0:4],"[")
-    return y
+    return y 
+
+
+def mcqfy(string):
+    a = list(mcqdictfy(string))
+    del a[0:4]
+    f = "["+"".join(a)
+    return f
